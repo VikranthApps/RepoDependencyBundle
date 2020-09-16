7bd947ec575cc7bf4a6a2e852cfa729e3fe82533
Vikranth Apps <imakingapps@gmail.com>
2020-09-16 13:17:15 +0530

    repo for bundle

---
+++ BundleWithDependency.framework/Headers/BundleWithDependency.h
@@ -0,0 +1,19 @@
+//
+//  BundleWithDependency.h
+//  BundleWithDependency
+//
+//  Created by Vikranth Naidu on 15/09/20.
+//  Copyright © 2020 Vikranth Naidu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BundleWithDependency.
+FOUNDATION_EXPORT double BundleWithDependencyVersionNumber;
+
+//! Project version string for BundleWithDependency.
+FOUNDATION_EXPORT const unsigned char BundleWithDependencyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BundleWithDependency/PublicHeader.h>
+
+
